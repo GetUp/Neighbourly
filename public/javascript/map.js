55b922e3b15ec1a1635af45ebdfb5ff75c95daf0
Tim McEwan <tim@mcewan.it>
2019-02-07 00:21:41 +1100

    Incorporate linear doors remaining ratio into priority

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -58,11 +58,16 @@ function makeMap() {
 
   FindLocation();
 
+  function normalise(val, min, max) { return (val - min) / (max - min) }
+
   function priorityStyles(feature) {
-    var propensity = feature.properties.avg_swing_propensity
-    if (propensity < 0.2625) return claimStyles.firstQuartile
-    if (propensity < 0.3250) return claimStyles.secondQuartile
-    if (propensity < 0.3875) return claimStyles.thirdQuartile
+    var propensity = normalise(feature.properties.avg_swing_propensity, 0.2, 0.45) // observed range
+    var doors_knocked = feature.properties.outcomes_recorded || 0
+    var total_doors = feature.properties.total_addresses_on_block || 1
+    var priority = propensity * (1 - doors_knocked / total_doors)
+    if (priority <= 0.25) return claimStyles.firstQuartile
+    if (priority <= 0.50) return claimStyles.secondQuartile
+    if (priority <= 0.75) return claimStyles.thirdQuartile
     return claimStyles.fourthQuartile
   }
 
